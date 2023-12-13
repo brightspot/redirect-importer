55d6990a463ffc4ca2274287628eb7b507b15570
Max Safo <max.safo@brightspot.com>
2023-12-13 14:20:08 -0500

    Add necessary dependencies, VanityRedirectGoogleDriveFileConverter implementation, add GoogleCSVUtils and DefaultImplementationSupplier

---
+++ src/main/java/brightspot/redirectimporter/VanityRedirectGoogleDriveFileConverter.java
@@ -0,0 +1,85 @@
+package brightspot.redirectimporter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import brightspot.redirect.QueryStringOption;
+import brightspot.redirect.QueryStringOptionModify;
+import brightspot.redirect.QueryStringOptionPreserve;
+import brightspot.redirect.VanityRedirect;
+import brightspot.redirectimporter.utils.DefaultImplementationSupplier;
+import brightspot.redirectimporter.utils.GoogleCSVUtils;
+import com.google.common.base.Throwables;
+import com.psddev.cms.db.ExternalItemConverter;
+import com.psddev.google.drive.GoogleDriveFile;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanityRedirectGoogleDriveFileConverter extends ExternalItemConverter<GoogleDriveFile, VanityRedirect> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VanityRedirectGoogleDriveFileConverter.class);
+
+    @Override
+    public Collection<? extends VanityRedirect> convert(GoogleDriveFile source) {
+        try {
+            String googleMimeType = source.getMimeType();
+            if (googleMimeType.toLowerCase(Locale.ROOT).contains("spreadsheet")) {
+                return extractRedirects(source);
+            }
+        } catch (IOException exception) {
+            LOGGER.error("Could not download file: " + exception.getMessage());
+            Throwables.propagate(exception);
+        }
+        return Collections.emptyList();
+    }
+
+    private static Collection<VanityRedirect> extractRedirects(GoogleDriveFile source) throws IOException {
+
+        Collection<VanityRedirect> result = new ArrayList<>();
+
+        // Extract redirects from the spreadsheet using csv
+        CSVParser csvParser = GoogleCSVUtils.getCsv(source, true);
+
+        List<CSVRecord> records = csvParser.getRecords();
+
+        for (CSVRecord csvRecord : records) {
+
+            String localPath = csvRecord.get(0);
+            String newUrl = csvRecord.get(1);
+            String status = csvRecord.get(2);
+            String queryString = csvRecord.get(3);
+
+            VanityRedirect vanityRedirect = new VanityRedirect();
+            vanityRedirect.setLocalUrls(Collections.singleton(localPath));
+            vanityRedirect.setDestination(newUrl);
+
+            // If status equals 302, set to true, otherwise it is false
+            boolean temporary = status.equals("302");
+            vanityRedirect.setTemporary(temporary);
+
+            if (queryString.equalsIgnoreCase("preserve")) {
+                vanityRedirect.setQueryString(DefaultImplementationSupplier.createDefault(
+                        QueryStringOption.class,
+                        QueryStringOptionPreserve.class));
+            } else if (queryString.equalsIgnoreCase("ignore")) {
+                vanityRedirect.setQueryString(DefaultImplementationSupplier.createDefault(
+                        QueryStringOption.class,
+                        QueryStringOptionPreserve.class));
+            } else if (queryString.equalsIgnoreCase("modify")) {
+                vanityRedirect.setQueryString(DefaultImplementationSupplier.createDefault(
+                        QueryStringOption.class,
+                        QueryStringOptionModify.class));
+            }
+        }
+
+        // Return redirects
+        return result;
+    }
+}
+
---
+++ src/main/java/brightspot/redirectimporter/utils/GoogleCSVUtils.java
@@ -0,0 +1,145 @@
+package brightspot.redirectimporter.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.psddev.cms.tool.CmsTool;
+import com.psddev.dari.db.Singleton;
+import com.psddev.dari.util.IoUtils;
+import com.psddev.google.GoogleAccountAccess;
+import com.psddev.google.GoogleAccountAccessDatastoreFactory;
+import com.psddev.google.drive.GoogleDriveFile;
+import com.psddev.google.drive.GoogleDriveSettings;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.lang3.StringUtils;
+
+public class GoogleCSVUtils {
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String APPLICATION_NAME = "brightspot/googledrive";
+
+    /**
+     * @param file Google drive file.
+     * @param hasHeader Whether the file has a header.
+     * Reads in the given <code>file</code> as {@link CSVParser}, either with the first record as the header row or with a
+     * {@link #generateDefaultHeaderRow} based on whether <code>hasHeader</code> exists.
+     *
+     * @param file Can't be null.
+     */
+    public static CSVParser getCsv(GoogleDriveFile file, boolean hasHeader) {
+        try (InputStream input = getFiles(file.getAccountAccess())
+                .export(file.getFileId(), "text/csv")
+                .executeMediaAsInputStream()) {
+
+            return CSVParser.parse(
+                    IoUtils.toString(input, StandardCharsets.UTF_8),
+                    hasHeader
+                            ? CSVFormat.DEFAULT.withAllowMissingColumnNames().withFirstRecordAsHeader()
+                            : CSVFormat.DEFAULT.withHeader(generateDefaultHeaderRow(file).toArray(new String[0])));
+
+        } catch (IOException error) {
+            throw new RuntimeException(
+                    "Can't export the Google Drive file into CSV!",
+                    error);
+        }
+    }
+
+    /**
+     * Constructs the {@link Drive} object by building a {@link com.google.api.client.auth.oauth2.Credential} and an {@link HttpTransport}.
+     *
+     * @param accountAccess Account that API requests will be made to
+     * @return {@link Drive} object from the Google SDK that will be used to make requests
+     */
+    public static Drive initializeApi(GoogleAccountAccess accountAccess) throws GeneralSecurityException, IOException {
+        if (accountAccess == null) {
+            throw new IOException("Cannot initialize API, Google account access cannot be retrieved");
+        }
+
+        GoogleAuthorizationCodeFlow flow = buildAuthFlow();
+        Credential credential = flow.loadCredential(accountAccess.getId().toString());
+
+        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+
+        return new Drive.Builder(transport, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    /**
+     * Returns all the files accessible by this {@link GoogleAccountAccess}.
+     */
+    public static Drive.Files getFiles(GoogleAccountAccess access) {
+        try {
+            return initializeApi(access).files();
+
+        } catch (GeneralSecurityException | IOException error) {
+            throw new IllegalStateException(
+                    "Can't initialize the Google Drive API!",
+                    error);
+        }
+    }
+
+    public static int getNumberOfColumns(GoogleDriveFile file) {
+        return Optional.of(getHeaderRow(file)).map(List::size).orElse(0);
+    }
+
+    /**
+     * Generates column heading strings based on the number of columns there are by manipulating their ASCII codes.
+     * Column headings follow the pattern A, B, C, …, Z, AA, AB, AC, …, AZ, BA, BB, …, ZZ, AAA, AAB, …, etc.
+     */
+    public static String generateColumnHeading(int i) {
+        return i < 0 ? "" : generateColumnHeading((i / 26) - 1) + (char) (65 + i % 26);
+    }
+
+    public static List<String> getHeaderRow(GoogleDriveFile file) {
+        return getCsv(file, true).getHeaderNames();
+    }
+
+    private static List<String> generateDefaultHeaderRow(GoogleDriveFile file) {
+        List<String> headers = new ArrayList<>();
+        for (int i = 0; i < getNumberOfColumns(file); i++) {
+            headers.add(generateColumnHeading(i));
+        }
+        return headers;
+    }
+
+    /**
+     * Builds the authorization flow for Google API Requests.
+     *
+     * @return Authorization flow for Google API Requests
+     */
+    static GoogleAuthorizationCodeFlow buildAuthFlow() throws IOException, GeneralSecurityException {
+        GoogleDriveSettings settings = Singleton.getInstance(CmsTool.class).as(GoogleDriveSettings.class);
+        String clientId = settings.getClientId();
+        String clientSecret = settings.getClientSecret();
+        if (StringUtils.isBlank(clientId) || StringUtils.isBlank(
+                clientSecret)) {
+            throw new IOException("Google Federated Search Client ID and Client Secret are not set in Sites & Settings");
+        }
+        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+        return new GoogleAuthorizationCodeFlow.Builder(
+                transport,
+                JSON_FACTORY,
+                clientId,
+                clientSecret,
+                DriveScopes.all())
+                .setAccessType("offline")
+                .setApprovalPrompt("force")
+                .setDataStoreFactory(new GoogleAccountAccessDatastoreFactory())
+                .build();
+    }
+}
